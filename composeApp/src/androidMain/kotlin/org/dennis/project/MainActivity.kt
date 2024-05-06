282a9194878106f1e2d57c4a381ee23c42ae6dd0
SirDennis <lilyngure@gmail.com>
2024-05-06 18:16:48 +0300

    library  set up

--- composeApp/src/androidMain/kotlin/org/dennis/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/dennis/project/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Set up Library and test from  here
+
         setContent {
             App()
         }
