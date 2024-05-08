c26db654db603e756c8fcdb88ef9e36c77a02319
SirDennis <lilyngure@gmail.com>
2024-05-08 15:59:50 +0300

    lib configured

--- multiplatformContact/src/commonMain/kotlin/multiContacts/MultiContacts.kt
+++ multiplatformContact/src/commonMain/kotlin/multiContacts/MultiContacts.kt
@@ -1,14 +1,16 @@
 package multiContacts
 
+import TimerManager
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
 @Composable
 public fun MultiContacts(
     modifier: Modifier = Modifier,
 ){
-
+    val timerManager = remember { TimerManager() }
     Text("Contacts")
 
 }
