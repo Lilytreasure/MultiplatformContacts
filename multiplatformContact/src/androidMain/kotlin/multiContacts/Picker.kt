9b58f84867be9784c41da7433fc7e4fadd45e006
SirDennis <lilyngure@gmail.com>
2024-05-29 12:51:55 +0300

    modified contacts Picker

--- multiplatformContact/src/androidMain/kotlin/multiContacts/Picker.kt
+++ multiplatformContact/src/androidMain/kotlin/multiContacts/Picker.kt
@@ -2,161 +2,115 @@ package multiContacts
 
 import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.provider.ContactsContract
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import android.content.pm.PackageManager
-import android.database.Cursor
 
+
+@RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
 @Composable
 actual fun pickLoaded() {
     val context = LocalContext.current
     val currentActivity: AppCompatActivity = (context as AppCompatActivity)
-    val singlePhoneNumberPickerLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.PickContact(),
-        onResult = { contactUri ->
-            contactUri?.let { uri ->
-                // Resolve the contact URI to get the phone number
-                println("Result from picked contacts;;;;;;" + uri.query.toString())
-
-                val phoneNumber = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    getPhoneNumberFromUri(context, uri)
-                } else {
-                    TODO("VERSION.SDK_INT < JELLY_BEAN")
-                }
-                // Handle the retrieved phone number
-                if (!phoneNumber.isNullOrEmpty()) {
-
-                    println("Loaded phone Number;;;;" + phoneNumber)
-
-                }
-            }
-        }
-    )
-    Button(onClick = {
-        if (ContextCompat.checkSelfPermission(
-                currentActivity,
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
-                    Manifest.permission.READ_CONTACTS
-                } else {
-                    Manifest.permission.READ_CONTACTS
-                }
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            singlePhoneNumberPickerLauncher.launch()
-
-        } else {
-            ActivityCompat.requestPermissions(
-                currentActivity,
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
-                    arrayOf(Manifest.permission.READ_CONTACTS)
-                } else {
-                    arrayOf(Manifest.permission.READ_CONTACTS)
-                },
-                33
-            )
-        }
-
-    }) {
-        Text("Pick Contact")
+    val result = remember { mutableStateOf<Bitmap?>(null) }
+    val resultContacts = remember { mutableStateOf<Uri?>(null) }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+        result.value = it
     }
-}
-
-@RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
-fun getPhoneNumberFromUri(context: Context, contactUri: Uri): String? {
-    val cursor = context.contentResolver.query(contactUri, null, null, null, null)
-    cursor?.use { c ->
-        if (c.moveToFirst()) {
-            // Get the column index for the phone number
-            val phoneNumberIndex =
-                c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-            if (phoneNumberIndex != -1) {
-                // Check if the column exists in the Cursor
-                val phoneNumber = c.getString(phoneNumberIndex)
-                if (!phoneNumber.isNullOrBlank()) {
-                    return phoneNumber
-                } else {
-                    Log.e("getPhoneNumberFromUri", "Phone number is null or blank")
-                }
+    val launcherContacts =
+        rememberLauncherForActivityResult(ActivityResultContracts.PickContact()) {
+            resultContacts.value = it
+        }
+    var phoneNumber by remember { mutableStateOf<String?>(null) }
+    LaunchedEffect(resultContacts.value) {
+        resultContacts.value?.let { uri ->
+            phoneNumber = getPhoneNumberFromUriData(context, uri)
+        }
+    }
+    Column() {
+        Button(onClick = {
+            if (ContextCompat.checkSelfPermission(
+                    currentActivity,
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+                        Manifest.permission.READ_CONTACTS
+                    } else {
+                        Manifest.permission.READ_CONTACTS
+                    }
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                launcherContacts.launch()
             } else {
-                // Log an error if the column index is not found
-                Log.e(
-                    "getPhoneNumberFromUri",
-                    "Phone number index not found in Cursor: $contactUri"
+                ActivityCompat.requestPermissions(
+                    currentActivity,
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+                        arrayOf(Manifest.permission.READ_CONTACTS)
+                    } else {
+                        arrayOf(Manifest.permission.READ_CONTACTS)
+                    },
+                    33
                 )
             }
-        } else {
-            // Log an error if the Cursor is empty
-            Log.e("getPhoneNumberFromUri", "Cursor is empty")
+
+        }) {
+            Text(text = "Pick Number")
+        }
+        result.value?.let { image ->
+            Image(image.asImageBitmap(), null, modifier = Modifier.fillMaxWidth())
+        }
+        resultContacts.value?.let { text ->
+            phoneNumber?.let { Text(text = it) }
         }
     }
-    return null
 }
 
-fun getContacts(context: Context): List<Pair<String, String>> {
-    val contactsList = mutableListOf<Pair<String, String>>()
-
-    // Define the columns you want to retrieve
-    val projection = arrayOf(
-        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-        ContactsContract.CommonDataKinds.Phone.NUMBER
-    )
-
-    // Query the contacts database
+fun getPhoneNumberFromUriData(context: Context, uri: Uri): String? {
+    val contentResolver = context.contentResolver
     val cursor: Cursor? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
-        context.contentResolver.query(
+        contentResolver.query(
             ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-            projection,
-            null,
-            null,
+            arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER),
+            "${ContactsContract.CommonDataKinds.Phone.CONTACT_ID} = ?",
+            arrayOf(uri.lastPathSegment),
             null
         )
     } else {
         TODO("VERSION.SDK_INT < ECLAIR")
     }
 
+    var phoneNumber: String? = null
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
         cursor?.use {
-            // Get the indexes of the columns
-            val nameIndex = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-            val phoneNumberIndex = it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
-
-            // Check if the indexes are valid
-            if (nameIndex != -1 && phoneNumberIndex != -1) {
-                // Iterate over the cursor to retrieve the data
-                while (it.moveToNext()) {
-                    val name = it.getString(nameIndex)
-                    val phoneNumber = it.getString(phoneNumberIndex)
-
-                    if (!phoneNumber.isNullOrBlank()) {
-                        contactsList.add(Pair(name, phoneNumber))
-                    } else {
-                        Log.e("getContacts", "Phone number is null or blank for contact: $name")
-                    }
-                }
-            } else {
-                if (nameIndex == -1) Log.e("getContacts", "Name index not found in Cursor")
-                if (phoneNumberIndex == -1) Log.e(
-                    "getContacts",
-                    "Phone number index not found in Cursor"
+            if (it.moveToFirst()) {
+                phoneNumber = it.getString(
+                    it.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER)
                 )
             }
-        } ?: run {
-            Log.e("getContacts", "Cursor is null")
         }
     }
 
-    return contactsList
+    return phoneNumber
 }
