7a8cb69ad24b1adb1e4a5b216e7acc8b0470e68a
SirDennis <lilyngure@gmail.com>
2024-06-06 17:54:49 +0300

    ios picker

--- multiplatformContact/src/iosMain/kotlin/multiContacts/Picker.kt
+++ multiplatformContact/src/iosMain/kotlin/multiContacts/Picker.kt
@@ -2,17 +2,36 @@ package multiContacts
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import platform.Contacts.CNContact
+import platform.Contacts.CNContactFormatter
+import platform.Contacts.CNContactFormatterStyle
+import platform.ContactsUI.CNContactPickerDelegateProtocol
+import platform.ContactsUI.CNContactPickerViewController
+import platform.UIKit.UIApplication
+import platform.darwin.NSObject
 
 @Composable
 actual fun pickMultiplatformContacts(onResult: (String) -> Unit): Launcher {
     val launcherCustom: Launcher?
     //Todo--Ios contacts Picker Implementation
-
+    val picker = CNContactPickerViewController()
+    picker.delegate = object : NSObject(), CNContactPickerDelegateProtocol {
+        override fun contactPicker(picker: CNContactPickerViewController, didSelectContact: CNContact) {
+            val fullName = CNContactFormatter.stringFromContact(didSelectContact, CNContactFormatterStyle.CNContactFormatterStyleFullName)
+            //onContactPicked(fullName ?: "No Name")
+        }
+    }
     launcherCustom = remember {
         Launcher(onLaunch = {
+            UIApplication.sharedApplication.keyWindow?.rootViewController?.presentViewController(
+                picker,
+                true,
+                null,
+            )
 
         })
     }
 
     return launcherCustom
 }
+
