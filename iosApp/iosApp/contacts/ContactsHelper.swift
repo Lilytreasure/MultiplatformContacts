1d9aef4ce8cbf2db9aa31a0264912ff9abd1cdd7
SirDennis <lilyngure@gmail.com>
2025-03-06 17:51:32 +0300

    ios contacts loader Update

--- iosApp/iosApp/contacts/ContactsHelper.swift
+++ iosApp/iosApp/contacts/ContactsHelper.swift
@@ -6,14 +6,39 @@
 //  Copyright © 2025 orgName. All rights reserved.
 //
 import SwiftUI
+import ContactsUI
 
-@objc public class ContactsHelper: NSObject {
+@objc public class ContactsHelper: NSObject, CNContactPickerDelegate {
+    
+    private var callback: ((String) -> Void)?
+
     @objc public override init() {
         super.init()
     }
     
-    @objc public func loadContacts(){
+    @objc public func loadContacts(callback: @escaping (String) -> Void) {
         print("Contacts loader invoked")
+        self.callback = callback
+        
+        guard let rootVC = UIApplication.shared.windows.first?.rootViewController else {
+            print("Root view controller not found")
+            return
+        }
+        
+        let contactPicker = CNContactPickerViewController()
+        contactPicker.delegate = self
+        contactPicker.displayedPropertyKeys = [CNContactPhoneNumbersKey]
+        rootVC.present(contactPicker, animated: true, completion: nil)
     }
     
+    public func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        if let phoneNumber = contact.phoneNumbers.first?.value.stringValue {
+            print("Selected Phone Number: \(phoneNumber)")
+            callback?(phoneNumber)
+        }
+    }
+    
+    public func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
+        print("Contact selection cancelled")
+    }
 }
